e24eabbebc4833fd5a93d7eefc2a3c412e5f52a8
V E L Δ <evan.robin07@gmail.com>
2021-11-24 08:15:23 +0100

    Update unluac.bat

--- tools/UnLuac/unluac.bat
+++ tools/UnLuac/unluac.bat
@@ -1,14 +1,14 @@
 @echo off
 type %1
-cmd for /r %%~nxI in (cd %1, %CD%) do (
+cmd for /r %%~ni in (cd %1, %CD%) do (
     cmd set out = %%~ni
 )
 echo %1
 echo "%~dp0unluac_2021_08_29b.jar"
 cmd set preoutput = %1
-if defined %preoutput% (
+::if defined %preoutput% (
     echo %output%
     set out = %preoutput:~-3%
     java -jar "%~dp0unluac_2021_08_29b.jar" "%1" > "%1%.lua"
     pause
-)
+::)
