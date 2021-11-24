33a518767882d329a85bbd7f81bfa88be631e421
V E L Δ <evan.robin07@gmail.com>
2021-11-24 08:08:38 +0100

    modified something, still trying to patch this shitting batch

--- tools/UnLuac/unluac.bat
+++ tools/UnLuac/unluac.bat
@@ -1,11 +1,14 @@
-:: @echo off
+@echo off
 type %1
-for /r %%o in (cd %1, %CD%) do (
-    set o = %%~ni
+cmd for /r %%~nxI in (cd %1, %CD%) do (
+    cmd set out = %%~ni
 )
 echo %1
 echo "%~dp0unluac_2021_08_29b.jar"
-echo %output%
-set preoutput = %1
-set out = %preoutput:~-3%
-java -jar "%~dp0unluac_2021_08_29b.jar" "%1" > "%1%.lua"
+cmd set preoutput = %1
+if defined %preoutput% (
+    echo %output%
+    set out = %preoutput:~-3%
+    java -jar "%~dp0unluac_2021_08_29b.jar" "%1" > "%1%.lua"
+    pause
+)
