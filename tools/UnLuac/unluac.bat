757ea522c546f30265ddaef3b404335ae04d2cce
V E L Δ <evan.robin07@gmail.com>
2021-11-25 22:31:37 +0100

    added some descs to the luac

--- tools/UnLuac/unluac.bat
+++ tools/UnLuac/unluac.bat
@@ -1,11 +1,14 @@
 @echo off
+
 type %1
-::cmd for /r %%~n1 in (cd %1, %CD%) do (
-::    cmd set out = %~n1
-::)
+:: Récupère le fichier droppé et le met dans la variable %1
+
 echo %1
 echo "%~dp0unluac_2021_08_29b.jar"
 echo "%~dp1%~n1.lua"
-set preoutput = %1
+:: Print les infos dans la console
+
 java -jar "%~dp0unluac_2021_08_29b.jar" "%1" > "%~dp1%~n1.lua"
-pause
+:: %~dp0 = %~ (Début de variable) d = directory (lieux) p = path (chemin d'accès) 0 = fichier initié (unluac.bat)
+:: %~dp1 = directory path 1 (Chemin d'accès au répertoire du fichier initié "1" donc le fichier drop)
+:: %~n1 = name 1 (Nom du fichier "1" donc nom du fichier drop)
